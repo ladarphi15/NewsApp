680e8b7459ff024963f0500f92c8cecb5eae2f68
hammer14 <angelina.hammer@edu.fh-joanneum.at>
2017-12-25 14:06:14 +0100

    Widget Extension

---
+++ app/src/main/java/at/fh_joanneum/newsly/news4u/WidgetProvider.java
@@ -0,0 +1,24 @@
+package at.fh_joanneum.newsly.news4u;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Angelina on 25.12.2017.
+ * https://www.youtube.com/watch?v=n2xViriT5nU
+ *
+ */
+
+public class WidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+}
