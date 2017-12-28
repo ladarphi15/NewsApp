47ce15ef5eadbaf1982125a6c327621aa4e36938
hammer14 <angelina.hammer@edu.fh-joanneum.at>
2017-12-28 23:35:22 +0100

    Share Button Prototype... dosn't work yet

--- app/src/main/java/at/fh_joanneum/newsly/news4u/NewsWebViewActivity.java
+++ app/src/main/java/at/fh_joanneum/newsly/news4u/NewsWebViewActivity.java
@@ -1,28 +1,97 @@
 package at.fh_joanneum.newsly.news4u;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.support.v7.widget.ShareActionProvider;
 
 import at.fh_joanneum.newsly.news4u.parser.RssEntry;
 
 /**
  * Created by aneuh on 06.05.2017.
  */
-
+//
 public class NewsWebViewActivity extends AppCompatActivity {
     public static RssEntry CURRENT_RSS_ENTRY = null;
+    private ShareActionProvider mShareActionProvider;
+    WebView webView;
+
+
+
+    String shareActionString;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_web);
-        WebView webView = (WebView) findViewById(R.id.rssWebView);
+        webView = (WebView) findViewById(R.id.rssWebView);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(CURRENT_RSS_ENTRY.getLink());
 
+
         ViewHelper.formatAppHeader(this);
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       getMenuInflater().inflate(R.menu.menu_start_screen, menu);
+
+        MenuItem shareMenuItem = menu.findItem(R.id.menu_item_share);
+
+        if (shareMenuItem != null) {
+            ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareMenuItem);
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "http://www.google.com");
+            shareActionProvider.setShareIntent(shareIntent);
+            //shareMenuItem.setIcon(R.drawable.ic_action_share);
+        }
+
+        return super.onCreateOptionsMenu(menu);
+        }
+        */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        /** Inflating the current activity's menu with res/menu/items.xml */
+        getMenuInflater().inflate(R.menu.menu_start_screen, menu);
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    /** Returns a share intent */
+    private Intent getDefaultShareIntent(){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "SUBJECT");
+        intent.putExtra(Intent.EXTRA_TEXT,"Extra Text");
+        return intent;
+    }
+
+    @Override
+    public final boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.share:
+                shareURL();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void shareURL() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, webView.getUrl());
+        startActivity(Intent.createChooser(shareIntent, "Share This!"));
     }
 }
--- app/src/main/java/at/fh_joanneum/newsly/news4u/StartScreenActivity.java
+++ app/src/main/java/at/fh_joanneum/newsly/news4u/StartScreenActivity.java
@@ -1,13 +1,18 @@
 package at.fh_joanneum.newsly.news4u;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class StartScreenActivity extends AppCompatActivity {
+
+    private ShareActionProvider mShareActionProvider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,26 +44,34 @@ public class StartScreenActivity extends AppCompatActivity {
             }
         });
     }
+/*
+Für Testzwecke share Option auf StartScreenActivity.
+Sobald share Funktionsfähig wird sie auf NewsWebView Übertragen
+ToDo: Share option via Whatsapp, fb, etc...
+Bis jetzt nur per sms share fähig
+ */
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_start_screen, menu);
+    return true;
+}
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_start_screen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public final boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.share:
+                shareURL();
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+    private void shareURL() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        //shareIntent.putExtra(Intent.EXTRA_TEXT, web.getUrl());
+        startActivity(Intent.createChooser(shareIntent, "Share This!"));
+    }
+
+
+
 }
